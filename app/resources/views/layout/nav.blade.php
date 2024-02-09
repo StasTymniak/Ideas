a4f95ec6c1a3496185e9859f8aa8223e1b5f294e
StasTymniak <tymniaks@gmail.com>
2024-02-09 17:29:19 +0200

    Admin panel

--- app/resources/views/layout/nav.blade.php
+++ app/resources/views/layout/nav.blade.php
@@ -12,17 +12,25 @@
             <ul class="navbar-nav">
                 @guest
                     <li class="nav-item">
-                        <a class="{{ Route::is('login') ? 'active text-decoration-underline' : '' }} nav-link " aria-current="page"
-                            href="{{ route('login') }}">Login</a>
+                        <a class="{{ Route::is('login') ? 'active text-decoration-underline' : '' }} nav-link "
+                            aria-current="page" href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="{{ Route::is('register') ? 'active text-decoration-underline' : '' }} nav-link" href="{{ route('register') }}">Register</a>
+                        <a class="{{ Route::is('register') ? 'active text-decoration-underline' : '' }} nav-link"
+                            href="{{ route('register') }}">Register</a>
                     </li>
                 @endguest
 
                 @auth
+                    @if (auth()->user()->is_admin)
+                        <li class="nav-item">
+                            <a class="{{ Route::is('admin.dashboard') ? 'active' : '' }} nav-link"
+                                href="{{ route('admin.dashboard') }}">Admin panel</a>
+                        </li>
+                    @endif
                     <li class="nav-item">
-                        <a class="{{ Route::is('profile') ? 'active text-decoration-underline' : '' }} nav-link" href="{{ route('profile') }}">{{ Auth::user()->email }}</a>
+                        <a class="{{ Route::is('profile') ? 'active ' : '' }} nav-link"
+                            href="{{ route('profile') }}">{{ Auth::user()->email }}</a>
                     </li>
                     <li class="nav-item">
                         <form action="{{ route('logout') }}" method="post">
--- app/routes/web.php
+++ app/routes/web.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\IdeaLikeContoller;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use Illuminate\Support\Facades\Route;
+
+
 
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
@@ -31,15 +34,15 @@ Route::group(['prefix' => 'ideas/', 'as' => 'ideas.',], function () {
 });
 
 Route::resource('users', UserController::class)->only('show');
-Route::resource('users', UserController::class)->only('edit','update')->middleware('auth');
+Route::resource('users', UserController::class)->only('edit', 'update')->middleware('auth');
 
-Route::get('profile', [UserController::class,'profile'])->middleware('auth')->name('profile');
+Route::get('profile', [UserController::class, 'profile'])->middleware('auth')->name('profile');
 
-Route::post('users/{user}/follow',[FollowerController::class,'follow'])->middleware('auth')->name('users.follow');
-Route::post('users/{user}/unfollow',[FollowerController::class,'unfollow'])->middleware('auth')->name('users.unfollow');
+Route::post('users/{user}/follow', [FollowerController::class, 'follow'])->middleware('auth')->name('users.follow');
+Route::post('users/{user}/unfollow', [FollowerController::class, 'unfollow'])->middleware('auth')->name('users.unfollow');
 
-Route::post('ideas/{idea}/like',[IdeaLikeContoller::class,'like'])->middleware('auth')->name('ideas.like');
-Route::post('ideas/{idea}/unlike',[IdeaLikeContoller::class,'unlike'])->middleware('auth')->name('ideas.unlike');
+Route::post('ideas/{idea}/like', [IdeaLikeContoller::class, 'like'])->middleware('auth')->name('ideas.like');
+Route::post('ideas/{idea}/unlike', [IdeaLikeContoller::class, 'unlike'])->middleware('auth')->name('ideas.unlike');
 
 Route::get('/feed', FeedController::class)->middleware('auth')->name('feed');
 
@@ -55,6 +58,10 @@ Route::get('/feed', FeedController::class)->middleware('auth')->name('feed');
 Route::get('/terms', function () {
     return view('terms');
 })->name('terms');
+
+Route::get('/admin', [AdminDashboardController::class, 'index'])->name('admin.dashboard')
+    ->middleware(['auth', 'admin']);
+
 
 
 
